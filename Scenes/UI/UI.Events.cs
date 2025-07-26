2fac5b3421c43d987977ea634b69196538f01881
Tati <iced.tati@gmail.com>
2025-07-26 16:48:32 -0400

    UI Events

---
+++ Scenes/UI/UI.Events.cs
@@ -0,0 +1,17 @@
+﻿using OBus;
+
+namespace UIEvents {
+  public class UITransitionEv: TEvent<UITransition> { }
+  public class UITransition(UIState from, UIState to) : Args {
+    public UIState from{ get; init; } = from;
+    public UIState to { get; init; } = to;
+  }
+  public enum UIState {
+    IDLE,
+    CAN_PICK_UP,
+    CAN_READ, 
+    //CAN_INTERACT
+    IS_HOLDING,
+    DIALOG,
+  }
+}
