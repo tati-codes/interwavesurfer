cf73a04a364e1e85f209ce0fcdc6bfe8a09b8595
Tati <iced.tati@gmail.com>
2025-08-10 16:58:37 -0400

    button and page animation, abstracting inkstate out from the quiz state

--- BaseClasses/GlobalState.cs
+++ BaseClasses/GlobalState.cs
@@ -14,21 +14,32 @@ using TatiDebug;
 using UIEvents;
 
 public partial class GlobalState : Node {
-	public Bus bus;
+	private Bus bus;
 
 	public bool PlayerCanInteract = true;
 	public bool PlayerIsHoldingItem { get; private set;  } = false;
 	public NodeRef InteractionTarget { get; private set;  } = null;
 	public Quiz QuizState { get; private set; }
+	public PC PCState { get; private set; }
+	public sceneEnum currentScene = sceneEnum.PC;
 	public override void _Ready() {
 		bus = GetNode<Bus>("/root/bus");
 		QuizState = new(bus);
+		PCState = new PC(bus);
 		subscribeToQuizEvents();
+		subscribeToPCEvents();
 		bus.Subscribe<PickupItem, PickupableItem>(args => PlayerIsHoldingItem = true);
 		bus.Subscribe<DropItem, DropItemArgs>(args => PlayerIsHoldingItem = false);
 		bus.Subscribe<LookingAt<ReadableItem>,ReadableItem>(args => InteractionTarget = args);
 		bus.Subscribe<LookingAt<PickupableItem>, PickupableItem>(args => InteractionTarget = args);
 		bus.Subscribe<StoppedLookingAt, NodeRef>(args => InteractionTarget = null);
+	}
+
+	private void subscribeToPCEvents() {
+		bus.Subscribe<StoryUpdated, StoryText>(PCState.update);
+		bus.Subscribe<ChoiceRequired, InkChoices>(PCState.update);
+		bus.Subscribe<UIItemHighlighted, SelectionIdx>(PCState.update);
+		bus.Subscribe<InkTagUpdated, InkTag>(PCState.update);
 	}
 
 	public void subscribeToQuizEvents() {
@@ -41,6 +52,12 @@ public partial class GlobalState : Node {
 }
 
 namespace State {
+public enum sceneEnum {
+		QUIZ,
+		PC,
+		ISLAND,
+		HOUSE
+}
 	public class Quiz {
 		public string currentText = "";
 		public string currentTag = "laur";
@@ -51,7 +68,8 @@ namespace State {
 		public Quiz(Bus _bus) {
 			bus = _bus;
 		}
-		public void broadcastDebug() => bus.Publish<DebugText, Text>(new Text(this.ToString()) { tag = TatiDebug.DebugData.debug_tag });
+		public void broadcastDebug() =>  bus.Publish<DebugText, Text>(new Text(this.ToString()) { tag = TatiDebug.DebugData.debug_tag }) ;
+		
 		public void update(StoryText text) {
 			this.currentText = text.Text;
 			broadcastDebug();
@@ -78,6 +96,50 @@ namespace State {
 		}
 		public override string ToString() {
 			return $"Quiz.CurrentText: {currentText}\n Quiz.CurrentChoices: {string.Join(" ,", currentChoices.Select(choice => $"{choice.Text} / {choice.Index}"))}\n Quiz.SelectedChoice: {selectedChoice?.Text + "/" + selectedChoice?.Index}";
+		}
+	}
+
+	public class PC : InkState {
+		public PC(Bus _bus) : base(_bus) { }
+	}
+
+	public class InkState {
+		public string currentText = string.Empty;
+		public string currentTag = string.Empty;
+		// public string currentTag = string.Empty;
+		public List<InkChoice> currentChoices = new List<InkChoice>();
+		public InkChoice? selectedChoice = null;
+		public Bus bus;
+		public bool debug = false;
+		public void update(InkChoices args) {
+			this.currentChoices = args.choices;
+			this.currentText = args.line;
+			this.selectedChoice = args.choices[0];
+			broadcastDebug();
+		}
+		public void update(StoryText text) {
+			this.currentText = text.Text;
+			broadcastDebug();
+		}
+		public void update(SelectionIdx args) {
+			if (currentChoices.Count > args.index) {
+				selectedChoice = currentChoices[args.index];
+			} else {
+				bus.Error("Fucked up UI Selection: " + args.index);
+				bus.Error("CurrentChoices: " + currentChoices.Count);
+				throw new Exception("Fucked up UI selection");
+			}
+			broadcastDebug();
+		}
+		// public void broadcastDebug() => 
+		// 	(debug ? bus.Publish<DebugText, Text>(new Text(this.ToString()) { tag = TatiDebug.DebugData.debug_tag }) : return);
+		public void broadcastDebug() { if (debug) bus.Publish<DebugText, Text>(new Text(this.ToString()) { tag = TatiDebug.DebugData.debug_tag }); }
+		public void update(InkTag args) {
+			this.currentTag = args.newTag;
+		}
+		
+		public InkState(Bus _bus) {
+			bus = _bus;
 		}
 	}
 }
--- Scenes/UI/FullScreenText.cs
+++ Scenes/UI/FullScreenText.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using AnimationEvents;
 using InkBridge;
 using OBus;
 using UIEvents;
@@ -11,13 +12,17 @@ public partial class FullScreenText : Control {
 	public FullScreenTextHolder LabelContainer {get; set;} 
 	[Export]
 	public Label ChoiceLabel {get; set;}
-
+	[Export]
+	public PanelContainer ChoiceContainer {get; set;}
+	
 	private const string INACTIVE_TAG = "general";
 	private const string FULLSCREEN_ACTIVE_TAG = "laur";
+	private Color default_modulate; 
 	bool active => global.QuizState.currentTag == FULLSCREEN_ACTIVE_TAG;
 	public override void _Ready()	{
 		bus = GetNode<Bus>("/root/bus");
 		global = GetNode<GlobalState>("/root/Global");
+		default_modulate = ChoiceContainer.Modulate;
 		bus.Subscribe<IShowDialogChoices, ChoiceDialogArgs>(args => {
 			if (!active) return;
 			LabelContainer.Append(args.content);
@@ -32,5 +37,19 @@ public partial class FullScreenText : Control {
 				LabelContainer.Reset();
 			}
 		});
+		bus.Subscribe<AnimationFinished, AnimationName>(arg => {
+			if (arg.name == "WhiteOutFrom3DSpace") {
+				animate_button_in();
+			} else if (arg.name == "DissolveFromFullscreen") {
+				animate_button_out();
+			}
+		});
+	}
+
+	void animate_button_in() => animate_button(new Color("ffffff"));
+	void animate_button_out() => animate_button(new Color("ffffff00"));
+	void animate_button(Color color) {
+		Tween tween = GetTree().CreateTween();
+		tween.TweenProperty(ChoiceContainer, "modulate", color, 1.0f);
 	}
 }
